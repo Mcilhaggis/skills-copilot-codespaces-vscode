f75a0ae2157a5a27c0bb94f3649776b5bf403763
Rachael McIlhagga <rachael.mcilhagga@live.co.uk>
2025-03-18 01:40:25 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,18 +1,2 @@
 //create web server
-const express = require('express');
-const app = express();
-const path = require('path');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const Comment = require('./models/comment');
 
-// Connect to MongoDB
-mongoose.connect('mongodb://mongodb:27017/comments', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', function () {
-  console.log('Connected to MongoDB');
-});
