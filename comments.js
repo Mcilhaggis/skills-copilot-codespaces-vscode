e3fabb506e1ee1e3b8f04a32a1dacc208d370591
Rachael McIlhagga <rachael.mcilhagga@live.co.uk>
2025-03-18 01:41:19 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,2 +1,37 @@
 //create web server
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
+//connect to mongodb
+mongoose.connect('mongodb://localhost:27017/comments', { useNewUrlParser: true, useUnifiedTopology: true });
+
+//create comment schema
+const commentSchema = new mongoose.Schema({
+    name: String,
+    comment: String,
+    date: { type: Date, default: Date.now }
+});
+
+//create comment model
+const Comment = mongoose.model('Comment', commentSchema);
+
+//use body-parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//use cors middleware
+app.use(cors());
+
+//get all comments
+app.get('/comments', (req, res) => {
+    Comment.find({}, (err, comments) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).json(comments);
+        }
+    });
+});
